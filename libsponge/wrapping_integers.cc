b0033507e387a2a7d7f29bf14386fd19b05a18f7
YangDong2002 <yangdong2002@stu.xjtu.edu.cn>
2020-09-30 20:15:01 +0800

    Finished WrappingInt32

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,7 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+	return isn + static_cast<uint32_t>(n);
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +28,7 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+	int32_t off = ((n - isn) - static_cast<uint32_t>(checkpoint));
+	if(off < 0 && checkpoint + off > checkpoint) return checkpoint + ((1ULL << 32) + off);
+	return off + checkpoint;
 }
